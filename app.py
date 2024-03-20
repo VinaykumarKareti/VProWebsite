f64a0dd98d803300a192fbbff23d4da254697e6f
Kareti Venkata Vinay Kumar <21pa1a0574@vishnu.edu.in>
2024-03-20 13:13:31 +0530

    Vinay1

--- app.py
+++ app.py
@@ -3,20 +3,19 @@ from pytube import YouTube
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=["GET"])
 def index():
-    return "Hi ra vny"
-#     return render_template('index.html')
+    return render_template('index.html')
 
-# @app.route('/download', methods=['POST'])
-# def download():
-#     video_url = request.form['video_url']
-#     resolution = request.form['resolution']
+@app.route('/download', methods=['POST'])
+def download():
+    video_url = request.form['video_url']
+    resolution = request.form['resolution']
 
-#     try:
-#         yt = YouTube(video_url)
-#         stream = yt.streams.filter(res=resolution).first()
-#         stream.download()
-#         return redirect(url_for('index'))
-#     except Exception as e:
-#         return render_template('error.html', error=str(e))
+    try:
+        yt = YouTube(video_url)
+        stream = yt.streams.filter(res=resolution).first()
+        stream.download()
+        return redirect(url_for('index'))
+    except Exception as e:
+        return render_template('error.html', error=str(e))
